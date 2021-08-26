13609b3edd45671a15629d79824ed8d3e4a208e4
Alex Kio <alexandermartelkio@gmail.com>
2021-08-26 11:49:15 -0400

    Squashed commit of the following:
    commit a76ff29d6aa291429ffcfaba1e82f98e95245f97
    Author: Alex Kio <alexandermartelkio@gmail.com>
    Date:   Thu Aug 26 11:48:50 2021 -0400
    
        Remove unnecessary type coercion in find response function
    
    commit f939f0ba5bd596df28736ae0412fd6f714cfaee9
    Merge: ced3a37 6143a98
    Author: Alex Kio <alexandermartelkio@gmail.com>
    Date:   Thu Aug 26 11:33:14 2021 -0400
    
        Merge branch 'main' into feature/initial-file-structure
    
    commit ced3a37dc412585995e195c40b37e41ca88ddf02
    Author: Alex Kio <alexandermartelkio@gmail.com>
    Date:   Thu Aug 26 11:29:51 2021 -0400
    
        Add PR Template
    
    commit 8dfed609abe861a1a6f6a2dbdf635b799b65bce8
    Author: Alex Kio <alexandermartelkio@gmail.com>
    Date:   Thu Aug 26 11:26:14 2021 -0400
    
        Update Readme
    
    commit f5b20edd0fea05fb92cea4c39ba5375162cbbc22
    Author: Alex Kio <alexandermartelkio@gmail.com>
    Date:   Thu Aug 26 11:26:06 2021 -0400
    
        Refactor index with Hash Router
    
    commit 6885c649f4eb75579457260aaad5664c01a47111
    Author: Alex Kio <alexandermartelkio@gmail.com>
    Date:   Thu Aug 26 11:25:49 2021 -0400
    
        Create fetch call
    
        to test error handling
    
    commit 688194abf6d30f4c555382ca4283d6a8c1fe4a74
    Author: Alex Kio <alexandermartelkio@gmail.com>
    Date:   Thu Aug 26 11:25:22 2021 -0400
    
        Create Header component
    
    commit 130026f7d3f9c457a00273ae22cef17b1dc162be
    Author: Alex Kio <alexandermartelkio@gmail.com>
    Date:   Thu Aug 26 11:25:11 2021 -0400
    
        Create Error component
    
    commit f0085ed2dda566420adf52ac218bd7bc29ecc80c
    Author: Alex Kio <alexandermartelkio@gmail.com>
    Date:   Thu Aug 26 11:24:35 2021 -0400
    
        Create Dashboard component
    
    commit e7765708b8ef1902b09fe13d63fbfbfe9fb8b5bd
    Author: Alex Kio <alexandermartelkio@gmail.com>
    Date:   Thu Aug 26 11:24:20 2021 -0400
    
        Create App component with Routing
    
    commit 85ac356d2640fa6f70bbc42b2bb0353ae0768c28
    Author: Alex Kio <alexandermartelkio@gmail.com>
    Date:   Thu Aug 26 11:23:55 2021 -0400
    
        Delete unused jest asset

---
+++ src/components/app/App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import './App.css';
+import { Header } from '../header/Header';
+import { Dashboard } from '../dashboard/Dashboard'
+import { Error } from '../error/Error'
+import { fetchData } from '../../utils/fetchCalls';
+
+export const App = () => {
+  const [fetchedData, setFetchedData] = useState([]);
+  const [errorCode, setErrorCode] = useState(0);
+  
+  const fetchAndCleanData = async () => {
+    try {
+      let fetchedData = await fetchData('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+      setFetchedData(fetchedData)
+    } catch (error) {
+      setErrorCode(Number(error.message))
+    }
+  }
+
+  useEffect(() => {
+    fetchAndCleanData()
+  }, [])
+
+  const clearSelected = () => {
+    setErrorCode(0)
+  }
+
+  return (
+    <>
+      <Header />
+      <main>
+        {errorCode !== 0 &&
+          <Error errorCode={errorCode} clearSelected={clearSelected} />
+        }
+        {!errorCode && (
+          <Switch>
+            <Route exact path='/' render={() =>
+              <Dashboard fetchedData={fetchedData} /> 
+            }/>
+
+            <Route exact path='/404' render={() =>
+              <Error errorCode={404} clearSelected={clearSelected} />
+            }/>
+            
+            <Redirect to='/404' />
+          </Switch>
+        )}
+      </main>
+    </>
+  );
+}
