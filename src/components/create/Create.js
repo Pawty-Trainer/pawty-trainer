77708d65a53e3f5b7d9c1d4dd91bced73660c1f5
Sydney Bear <sbear3915@gmail.com>
2021-08-28 10:51:51 -0600

    Add name input

--- src/components/create/Create.js
+++ src/components/create/Create.js
@@ -5,9 +5,19 @@ const Create = () => {
   const [name, setName] = useState('');
   const [age, setAge] = useState(0);
   const [breed, setBreed] = useState('')
-  
+
   return(
-    <h1>Train your dog!</h1>
+    <form>
+      <h1 className='train-dog-title'>Train your dog!</h1>
+      <input
+        type='text'
+        placeholder='Name'
+        name = 'name'
+        value= {name}
+      />
+      
+    </form>
+    
   )
 }
 
