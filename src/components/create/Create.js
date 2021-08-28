966dfe0a76f9247d80655ec8cde922315a22a91e
Sydney Bear <sbear3915@gmail.com>
2021-08-28 10:50:00 -0600

    Update component with state

--- src/components/create/Create.js
+++ src/components/create/Create.js
@@ -2,7 +2,10 @@ import './Create.css';
 import React, { useState, useEffect } from 'react';
 
 const Create = () => {
-  const [name, setName] = use
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [breed, setBreed] = useState('')
+  
   return(
     <h1>Train your dog!</h1>
   )
