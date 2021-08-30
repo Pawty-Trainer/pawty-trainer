7dc18df5d32a3ac80b1228d5c1ecd098eec9f6cf
Alex Kio <alexandermartelkio@gmail.com>
2021-08-29 22:05:21 -0400

    Squashed commit of the following:
    commit ebd4c50b2af51b6ad7d256077849585b4b6232f9
    Author: Alex Kio <alexandermartelkio@gmail.com>
    Date:   Sun Aug 29 22:04:02 2021 -0400
    
        Add styling to Dashboard
    
    commit 55106cb3d952ce943c04139072d3f09512d8abb8
    Author: Alex Kio <alexandermartelkio@gmail.com>
    Date:   Sun Aug 29 22:03:52 2021 -0400
    
        Hardcode Dashboard view
    
    commit 4cb6388f02857728239245cfc9c370c9d40f31bb
    Author: Alex Kio <alexandermartelkio@gmail.com>
    Date:   Sun Aug 29 22:03:35 2021 -0400
    
        Add base styling to App
    
    commit 8f8eede341a076436d7dbbade5753de35e414194
    Author: Alex Kio <alexandermartelkio@gmail.com>
    Date:   Sun Aug 29 21:43:32 2021 -0400
    
        Update nav semantics in Header
    
    commit d8578736c73281eec87150ac589264241ae13281
    Author: Alex Kio <alexandermartelkio@gmail.com>
    Date:   Sun Aug 29 21:35:13 2021 -0400
    
        Add app title header
    
    commit 5496024c2d9d349e2f2b029401a91c60f9969471
    Author: Alex Kio <alexandermartelkio@gmail.com>
    Date:   Sun Aug 29 21:19:55 2021 -0400
    
        Add styling for Header component
    
    commit 71be902f00fdad12927b503bfe8ca17881bdffb4
    Author: Alex Kio <alexandermartelkio@gmail.com>
    Date:   Sun Aug 29 21:13:39 2021 -0400
    
        Update NavLinks to Links for HashRouter
    
    commit cf61883f0edd3d1957df358eb7e095f40dda1a20
    Author: Alex Kio <alexandermartelkio@gmail.com>
    Date:   Sun Aug 29 21:13:18 2021 -0400
    
        Import Roboto font

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -26,8 +26,31 @@ export const Dashboard = ({ userID }) => {
   }
 
   if (data) {
+    console.log(data)
     return (
-      <p>Data...</p>
+      <section className="dashboard">
+        <h2>Welcome {data.user.name}!</h2>
+
+        <section>
+          <section>
+            <h3>My Dogs</h3>
+            <ul>
+              <li>Fifi</li>
+            </ul>
+          </section>
+
+          <section>
+            <h3>Upcoming Events</h3>
+            <dl>
+              <dt>Today</dt>
+              <dd>6pm - Sit with Fifi</dd>
+
+              <dt>Tomorrow</dt>
+              <dd>4pm - Play with Fifi</dd>
+            </dl>
+          </section>
+        </section>
+      </section>
     )
   }
 
