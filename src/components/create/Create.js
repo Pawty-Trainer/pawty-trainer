df2c5a412af729507d2d9a9864de51de081ae21d
Sydney Bear <sbear3915@gmail.com>
2021-09-02 16:38:20 -0600

    Update drop down to have a search functionality

--- src/components/create/Create.js
+++ src/components/create/Create.js
@@ -3,12 +3,13 @@ import React, { useState } from 'react';
 import { useMutation } from "@apollo/client";
 import { ADD_NEW_DOG } from '../../utils/graph_mutations';
 import { QUERY_EVERYTHING } from '../../utils/graph_queries';
+import Select from 'react-select'
+import { fetchBreeds } from '../../utils/apiCalls';
 
 const Create = ({ userID, breeds }) => {
   const [name, setName] = useState('');
   const [age, setAge] = useState('');
-  const [breed, setBreed] = useState('');
-  //Will eventually use api that backend creates to turn this into a drop down 
+  const [breedType, setBreed] = useState('');
   const [formError, setFormError] = useState('')
   const [addDog, { loading, error }] = useMutation(ADD_NEW_DOG, {
     refetchQueries: [QUERY_EVERYTHING]
@@ -16,12 +17,12 @@ const Create = ({ userID, breeds }) => {
 
   const submitDog = event => {
     event.preventDefault();
-    if(name && age && breed) {
+    if(name && age && breedType) {
       addDog({
         variables: {
           name: name,
           userId: userID,
-          breed: breed,
+          breed: breedType,
           age: Number(age)
         }
       });
@@ -42,6 +43,8 @@ const Create = ({ userID, breeds }) => {
   if (loading) return 'Submitting...';
   if (error) return `Submission error! ${error.message}`;
 
+  const options = breeds.map(breed=> ({label: breed.name, value: breed.name}))
+
   return(
     <form>
       <h1 className='train-dog-title'>Train your dog!</h1>
@@ -61,24 +64,31 @@ const Create = ({ userID, breeds }) => {
         onChange={(event)=> setAge(event.target.value)}
         required
       />
-      {/* <input
-        type='text'
-        placeholder='Breed'
-        name = 'breed'
-        value= {breed}
-        onChange={(event)=> setBreed(event.target.value)}
-        required
-      /> */}
-      <select>
+      <label>Select Breed:</label>
+      <Select className='select'
+        // onChange={(breed)=> setBreed(breed.target.value)}
+        options = {options}
+        onChange={(event) => setBreed(event.value)}
+      />
+        {/* <option 
+          placeholder='Dog Breed' 
+          value='Dog Breed'
+          >Dog Breed
+        </option>
+        <option 
+          value='Mixed'
+          >Mixed Breed
+        </option>
         {breeds.map(breed => (
           <option
-            key={breed.name}
+            label={breed.id}
             value={breed.name}
           >
             {breed.name}
           </option>
-        ))}
-      </select>
+        ))} */}
+        
+      {/* </Select> */}
       <button onClick={event => submitDog(event)}>Create</button>
       <p>{formError}</p>
     </form>
