940acc1255d5a2d70c988ada56ee4829f7aaede6
Sydney Bear <sbear3915@gmail.com>
2021-09-03 16:33:54 -0600

    Update colors

--- src/components/eventCard/EventCard.js
+++ src/components/eventCard/EventCard.js
@@ -20,19 +20,22 @@ export const EventCard = ({ event }) => {
   return (
     <section key={event.id} className="event-card">
       <dl>
-        <dt>Dog</dt>
-        <dd>{event.dog.name}</dd>
-
-        <dt>Event Name</dt>
-        <dd>
-          <Link to={`/event/${event.id}`} key={event.id} >{event.name}</Link>
-        </dd>
-
-        <dt>Event Date</dt>
-        <dd>{(new Date(event.eventDatetime)).toLocaleString()}</dd>
+        <div className='description-container'>
+          <dd class>{event.dog.name}</dd>
+        </div>
+        <div className='description-container'>
+          <dt className='card-title'>Event Name</dt>
+          <dd>
+            <Link to={`/event/${event.id}`} key={event.id} >{event.name}</Link>
+          </dd>
+        </div>
+        <div className='description-container'>
+          <dt className='card-title'>Event Date</dt>
+          <dd>{(new Date(event.eventDatetime)).toLocaleString()}</dd>
+        </div>
       </dl>
       <input type='checkbox' id='complete' name='complete' value='true' onClick={() => handleClick()}></input>
-        <label>Complete Event</label>
+        <label className='complete-title' >Complete Event</label>
     </section>
   )
 }
